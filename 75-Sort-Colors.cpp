01f41f48f40075af4526b6cd2417899d5d6823cb
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-02-03 22:15:01 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 91.63%] - LeetPush

--- 75-Sort-Colors.cpp
+++ 75-Sort-Colors.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-          int i=0;
-   int  k=0;
-   int j=nums.size()-1;
+        int i,j,k;
+        i=0;
+        j=nums.size()-1;
 
-   while(k<=j){
-     if (nums[k] == 0) {
-            swap(nums[k], nums[i]);
-            i++;
-            k++;
-        } else if (nums[k] == 2) {
-            swap(nums[k], nums[j]);
-            j--;
-        } else { // nums[k] == 1
-            k++;
+        k=0;
+
+        while(k<=j){
+            
+            if(nums[k]==0){
+                swap(nums[k],nums[i]);
+                i++;
+                k++;
+                
+            }
+            else if(nums[k]==2){
+                swap(nums[k],nums[j]);
+                j--;
+                
+            }else{
+                k++;
+            }
+           
+            
         }
-   }
     }
 };
