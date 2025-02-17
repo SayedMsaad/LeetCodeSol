c318e8e400bddaed89579b79adcf13600732f290
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-02-17 17:35:22 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 34.13%] - LeetPush

---
+++ 540-Single-Element-in-a-Sorted-Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int sum=0;
+        for(auto it:nums){
+            sum^=it;
+        }
+        return sum;
+    }
+};
