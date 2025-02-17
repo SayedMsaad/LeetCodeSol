ef0269df4347231fc801ea2c35da6c35b87a3a0d
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-02-17 19:09:58 +0530

    [88] [Time Beats: 4.15%] [Memory Beats: 70.76%] - LeetPush

--- 88-Merge-Sorted-Array.cpp
+++ 88-Merge-Sorted-Array.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> nums3;
-        int i = 0;
-        int j = 0;
+        
+        vector<int> arr;
+        int i=0,j=0;
 
-        if (nums1.size() == 1 && nums1[0] == 0) {
-            nums1.pop_back();
-            nums1.insert(nums1.begin(), nums2.begin(), nums2.end());
-            return;
-        } else if (n == 0) {
-            return;
-        } else {
-
-            while (i < m && j < n) {
-                if (nums1[i] <= nums2[j]) {
-                    nums3.push_back(nums1[i]);
-                    i++;
-
-                } else {
-                    nums3.push_back(nums2[j]);
-                    j++;
-                }
-            }
-            while (i < m) {
-                nums3.push_back(nums1[i]);
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){
+                arr.push_back(nums1[i]);
                 i++;
             }
-            while (j < n) {
-                nums3.push_back(nums2[j]);
+            else{
+                arr.push_back(nums2[j]);
                 j++;
             }
-            for (int i = 0; i < (n + m); i++) {
-                nums1[i] = nums3[i];
-            }
         }
+
+        while(i<m){
+            arr.push_back(nums1[i]);
+            i++;
+        }
+        while(j<n){
+            arr.push_back(nums2[j]);
+            j++;
+        }
+
+        for(int i=0;i<arr.size();i++){
+            nums1[i]=arr[i];
+        }
+
+
+
     }
 };
