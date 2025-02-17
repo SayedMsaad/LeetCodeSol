c62814dcd6aebec68b86569587a65dd3a5746cc4
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-02-17 21:09:13 +0530

    [26] [Time Beats: 100.00%] [Memory Beats: 50.70%] - LeetPush

--- 26-Remove-Duplicates-from-Sorted-Array.cpp
+++ 26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int count =1;
+        int j = 1 ;
 
-        int size= nums.size()-1;
-        vector<int>arr;
-        arr.push_back(nums[0]);
-        int j=0;
-        for(int i=0;i<=size;i++){
-            if(nums[i]!=nums[j]){
-            count ++;
-            j=i;
-            arr.push_back(nums[i]);
-            }
-            }
-        
-       
-    //    for(auto it:arr){
-    //     cout<<it;
-    //    }
+        for(int i =1 ;i < nums.size();i++){
+            if(nums[i] == nums[i-1]) continue;
 
-        nums.clear();
-        nums.insert(nums.end(),arr.begin(),arr.end());
-        return count;
+            swap(nums[i], nums[j]);
+            nums[i] = nums[j];
+            j++;
+        }
+        return  j;
     }
 };
