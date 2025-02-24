f5f1861f86cd61b33240cd88529c7df5f183dffa
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-02-24 22:13:56 +0530

    [852] [Time Beats: 100.00%] [Memory Beats: 78.79%] - LeetPush

--- 852-Peak-Index-in-a-Mountain-Array.cpp
+++ 852-Peak-Index-in-a-Mountain-Array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int s=0;
-        int e=arr.size()-1;
-        int mid= s +(e-s)/2;
-
-        while(s<e){
-            if(arr[mid]<arr[mid+1]){
-                s=mid+1;
-            }
-            else{
-                e=mid;
-            }
-
-            mid=s+(e-s)/2;
+        int low =0;
+        int high=arr.size()-1;
+      
+        if(arr[low]>arr[low+1]){
+            return arr[low];
+        }else{
+            low++;
         }
-        return e;
+
+        if(arr[high]>arr[high-1]){
+            return high;
+        }else{
+            high--;
+        }
+        while(low<high){
+            int mid =(low + high) / 2;
+
+            if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]){
+                low=mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return high;
     }
 };
