e10f77c88942303040b4907c058a4343ed09bff6
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-02-24 21:21:21 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 68.30%] - LeetPush

--- 540-Single-Element-in-a-Sorted-Array.cpp
+++ 540-Single-Element-in-a-Sorted-Array.cpp
@@ -1,10 +1,47 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int sum=0;
-        for(auto it:nums){
-            sum^=it;
+        int low=0;
+        int high=nums.size()-1;
+
+        if(nums.size()==1){
+            return nums[0];
         }
-        return sum;
+        
+            if(nums[low] != nums[low+1]){
+                return nums[low];
+            }else{
+                low++;
+            }
+
+            if(nums[high]!=nums[high-1]){
+                return nums[high];
+            }else{
+                high--;
+            }
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(nums[mid]!=nums[mid+1] && nums[mid]!= nums[mid-1] ){
+                return nums[mid];
+            }
+            
+            if(mid % 2==0){
+                if(nums[mid]!=nums[mid+1]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+        
+            }else{
+                if(nums[mid]!=nums[mid-1]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }
+
+        }
+        return -1;
     }
 };
