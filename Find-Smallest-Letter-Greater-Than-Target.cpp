464651170ce116c0e11535e7ab5b460233cb2322
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-05-16 22:22:23 +0530

    [744] [Time Beats: 100.00%] [Memory Beats: 99.08%] - LeetPush

---
+++ Find-Smallest-Letter-Greater-Than-Target.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int s=0;
+        int e= letters.size()-1;
+        int ans=letters[0];
+        while(s<=e){
+            int mid = (s+e)/2;
+            
+           if(letters[mid]>target){
+            ans=letters[mid];
+            e=mid-1;
+           }else{
+                s=mid+1;
+           }
+
+        }
+
+       return ans;
+    }
+};
