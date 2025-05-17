b60f3a158a7dcb10e34c6144ca33682c2709414b
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-05-18 02:37:16 +0530

    [120] [Time Beats: 100.00%] [Memory Beats: 11.03%] - LeetPush

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
 
-    int solve(vector<vector<int>> &arr,int i,int j,vector<vector<int>>&dp){
-        if(i==arr.size()-1){
+    int solve(vector<vector<int>> &arr,int i,int j,vector<vector<int>> &dp){
+        if(i == arr.size()-1){
             return arr[i][j];
         }
-        int ans=0;
         if(dp[i][j]!=-1){
-            return dp[i][j];
+         return dp[i][j];
+        }else{
+         return dp[i][j]=arr[i][j]+min(solve(arr,i+1,j,dp), solve(arr,i+1,j+1,dp));
         }
-        int left = solve(arr,i+1,j,dp);
-        int right = solve(arr,i+1,j+1,dp);
-        ans = arr[i][j]+min(left,right);
-        dp[i][j]=ans;
-        return ans;
+        
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int ans = 0;
-        int n = triangle.size();
-       vector<vector<int>> dp(n,vector<int>(n,-1));
+    int minimumTotal(vector<vector<int>>& tri) {
+      int ans = 0;
+      int n= tri.size();
+      vector<vector<int>> dp(n,vector<int>(n,-1));
 
-        ans = solve(triangle,0,0,dp);
-        // cout<<dp[2][1];
+      ans = solve(tri,0,0,dp);
 
-        return ans;
+      return ans;
     }
 };
