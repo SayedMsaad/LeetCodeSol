e556586d02833dc6358fe8f73c8e2866ee4da4fb
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-05-30 03:20:57 +0530

    [46] [Time Beats: 100.00%] [Memory Beats: 48.85%] - LeetPush

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    void solve(vector<int> &ds,vector<bool> &check,vector<int>&nums,vector<vector<int>> &ans){
-        if(ds.size() == nums.size()){
-            ans.push_back(ds);
+//approuch 1;
+    // void solve(vector<int> &ds,vector<bool> &check,vector<int>&nums,vector<vector<int>> &ans){
+    //     if(ds.size() == nums.size()){
+    //         ans.push_back(ds);
+    //         return;
+    //     }
+
+    //     for(int it=0;it<nums.size();it++){
+    //         if(!check[it]){
+    //             ds.push_back(nums[it]);
+    //             check[it]=true;
+    //             solve(ds,check,nums,ans);
+    //             ds.pop_back();
+    //             check[it]=false;
+    //         }
+    //     }
+    // }
+//approuch 2
+    void solve(int i,vector<int> &nums,vector<vector<int>> &ans){
+        if(i == nums.size()){
+            ans.push_back(nums);
             return;
         }
 
-        for(int it=0;it<nums.size();it++){
-            if(!check[it]){
-                ds.push_back(nums[it]);
-                check[it]=true;
-                solve(ds,check,nums,ans);
-                ds.pop_back();
-                check[it]=false;
-            }
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            solve(i+1,nums,ans);
+            swap(nums[i],nums[j]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
@@ -22,7 +36,7 @@ public:
         vector<int> ds;
         vector<bool> check(nums.size(),false);
         
-        solve(ds,check,nums,ans);
+        solve(0,nums,ans);
 
         return ans;
         
