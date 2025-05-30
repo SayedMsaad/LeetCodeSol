e3577037f2229e1d754f1ea707e861093ce71d1e
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-05-31 02:28:25 +0530

    [31] [Time Beats: 100.00%] [Memory Beats: 67.46%] - LeetPush

--- Next-Permutation.cpp
+++ Next-Permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int ind=-1;
+
+        //find the braking point of the array
+
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
+            }
+        }
+        cout<<ind;
+
+        if(ind == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[ind]<nums[i]){
+                swap(nums[ind],nums[i]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+ind+1,nums.end());
         
     }
 };
