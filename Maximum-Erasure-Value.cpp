bd3b72ffc5be56a418f64196cdad246d8754add2
SayedMsaad <145865179+SayedMsaad@users.noreply.github.com>
2025-07-22 23:40:09 +0530

    [1695] [Time Beats: 94.67%] [Memory Beats: 92.12%] - LeetPush

--- Maximum-Erasure-Value.cpp
+++ Maximum-Erasure-Value.cpp
@@ -6,22 +6,34 @@ public:
         unordered_set<int> s;
         int sum=0;
         int maxi=INT_MIN;
+        int lsum;
+        vector<int> mp(10001,-1);
 
-        while(r<nums.size()){
-            if(s.find(nums[r]) == s.end()){
-                sum+=nums[r];
-                s.insert(nums[r]);
-                maxi=max(maxi,sum);
-                r++;
-          }else{
-            while(s.find(nums[r])!=s.end()){
-                sum-=nums[l];
-                s.erase(nums[l]);
-                l++;
-            }
-          }
+        vector<int> csum(nums.size(),0);
+        csum[0]=nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            csum[i]=csum[i-1]+nums[i];
         }
 
+        while(r<nums.size()){
+            if(mp[nums[r]]==-1){
+                mp[nums[r]]=r;
+                lsum=l==0?0:csum[l-1];
+               sum=csum[r]-lsum;
+               maxi=max(sum,maxi);
+               r++;
+            }else{
+                l=max(l,mp[nums[r]]+1);
+                mp[nums[r]]=r;
+                lsum=l==0?0:csum[l-1];
+               sum=csum[r]-lsum;
+               maxi=max(sum,maxi);
+               r++;
+            }
+        }
+
+    
         return maxi;
 
     }
